2864ba2304ef15640562ccc45df9d5ea0d52da52
Siddharth Vaish <sidvaish97@gmail.com>
2025-02-20 19:02:42 +0530

    Improve

--- gen_pages.py
+++ gen_pages.py
@@ -1,26 +1,31 @@
 import mkdocs_gen_files
 import requests
 import os
+import re
 
 # GitHub raw URL for application.properties
-ODOO_OPENMRS_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-odoo-openmrs/da35c6587c4cf280d9baace5ad9eb8ea29d5b640/odoo-openmrs/src/main/resources/config/application.properties"
+ODOO_OPENMRS_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-odoo-openmrs/7773cdb2783c2b7a5310e49ec79e979a65f2c1e0/odoo-openmrs/src/main/resources/config/application.properties"
 OPENMRS_ORTHANC_GITHUB_URL = ""
 KEYCLOCK_SUPERSET_GITHUB_URL = ""
-OPENMRS_SENAITE_GITHUB_URL = ""
-ERPNEXT_OPENMRS_GITHUB_URL = ""
+OPENMRS_SENAITE_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-openmrs-senaite/7184244ee52db4df85efab9fed9c63a2d993cbe1/senaite-openmrs/src/main/resources/config/application.properties"
+ERPNEXT_OPENMRS_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-erpnext-openmrs/789020a15dbbb10f0bd5b2c994a1de2e82cf7ae7/erpnext-openmrs/src/main/resources/config/application.properties"
 
 mkdocs_config_points = {'mkdocs-config-name': 'Name', 'mkdocs-config-description': 'Description',
                         'mkdocs-config-location': 'Location', 'mkdocs-config-possible-values': 'Possible Values',
                         'mkdocs-config-default-value': 'Default Value'}
+
 mkdocs_block_end_identifier = '# /mkdocs-end'
 
-app_github_dict = {'Odoo-OpenMRS Flows': ODOO_OPENMRS_GITHUB_URL}
+app_github_dict = {'Odoo-OpenMRS Flows': ODOO_OPENMRS_GITHUB_URL, 'OpenMRS-SENAITE Flows': OPENMRS_SENAITE_GITHUB_URL,
+                   'ERPNext-OpenMRS Flows': ERPNEXT_OPENMRS_GITHUB_URL}
 
 
-# app_github_dict = {'Odoo-OpenMRS Flows': ODOO_OPENMRS_GITHUB_URL, 'OpenMRS-Orthanc Flows': OPENMRS_ORTHANC_GITHUB_URL,
-#                    'OpenMRS-Orthanc Flows': KEYCLOCK_SUPERSET_GITHUB_URL,
-#                    'OpenMRS-SENAITE Flows': OPENMRS_SENAITE_GITHUB_URL,
-#                    'ERPNext-OpenMRS Flows': ERPNEXT_OPENMRS_GITHUB_URL}
+def mk_example(example_heading, example_text):
+    component = '!!! example "' + example_heading + '" \n\n' + '    ```java\n    ' + example_text + '\n    ```'
+    return component
+
+
+mkdocs_components = {'mkdocs_component-mk-example': mk_example}
 
 
 def process_application_properties(text):
@@ -35,6 +40,9 @@ def process_application_properties(text):
         if 'mkdocs' in line.strip():
             for key, value in mkdocs_config_points.items():
                 line = line.replace('# /' + key + ':', mk_bold(value + ': '))
+            if 'mkdocs_component' in line:
+                for key, value in mkdocs_components.items():
+                    line = line.replace(line, populate_mk_docs_components(value, line))
             result_lines.append('\n' + line)
 
     return '\n'.join(result_lines)
@@ -64,6 +72,13 @@ def generate_file(lines, filename):
         print(''.join(lines), file=f)
 
 
+def populate_mk_docs_components(func, text):
+    print('kajsfdkhadsf ' + text)
+    values = re.findall(r"mk-arg\d+=(.*?)(?=\s*mk-arg\d+=|$)", text)  # Looks for mk-arg1, mk-arg2 etc
+    print(f"Expected at least 2 arguments for {func.__name__}, but got {len(values)}: {values}")
+    return func(*values)
+
+
 def page_heading(heading):
     return '# ' + heading
 
@@ -82,11 +97,6 @@ def mk_heading(text):
 
 def mk_ozone_pro_tag():
     component = '!!! info inline end ""\n' + '\n    {==:oz: Pro==}\n'
-    return component
-
-
-def mk_example(example_heading, example_text):
-    component = '!!! example "' + example_heading + '" \n\n' + '    ```java\n    ' + example_text + '\n    ```'
     return component
 
 
