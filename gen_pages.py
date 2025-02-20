99935bce6b262a1c3708e55e2b796d864964f1fe
Siddharth Vaish <sidvaish97@gmail.com>
2025-02-20 20:00:12 +0530

    Improve

--- gen_pages.py
+++ gen_pages.py
@@ -5,7 +5,7 @@ import re
 
 # GitHub raw URL for application.properties
 ODOO_OPENMRS_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-odoo-openmrs/7773cdb2783c2b7a5310e49ec79e979a65f2c1e0/odoo-openmrs/src/main/resources/config/application.properties"
-OPENMRS_ORTHANC_GITHUB_URL = ""
+OPENMRS_ORTHANC_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-openmrs-orthanc/45b2c6361da5afcb0bf281229f484e8bec7ae0f2/openmrs-orthanc/src/main/resources/config/application.properties"
 KEYCLOCK_SUPERSET_GITHUB_URL = ""
 OPENMRS_SENAITE_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-openmrs-senaite/7184244ee52db4df85efab9fed9c63a2d993cbe1/senaite-openmrs/src/main/resources/config/application.properties"
 ERPNEXT_OPENMRS_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-erpnext-openmrs/789020a15dbbb10f0bd5b2c994a1de2e82cf7ae7/erpnext-openmrs/src/main/resources/config/application.properties"
@@ -17,7 +17,7 @@ mkdocs_config_points = {'mkdocs-config-name': 'Name', 'mkdocs-config-description
 mkdocs_block_end_identifier = '# /mkdocs-end'
 
 app_github_dict = {'Odoo-OpenMRS Flows': ODOO_OPENMRS_GITHUB_URL, 'OpenMRS-SENAITE Flows': OPENMRS_SENAITE_GITHUB_URL,
-                   'ERPNext-OpenMRS Flows': ERPNEXT_OPENMRS_GITHUB_URL}
+                   'ERPNext-OpenMRS Flows': ERPNEXT_OPENMRS_GITHUB_URL,'OpenMRS-Orthanc Flows': OPENMRS_ORTHANC_GITHUB_URL}
 
 
 def mk_example(example_heading, example_text):
