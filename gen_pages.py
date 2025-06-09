f570317c535d5ab1c008030772e9b4ec64d9e3c8
Siddharth Vaish <sidvaish97@gmail.com>
2025-06-09 18:55:56 +0530

    OZ-558: Fix URL to the `eip-erpnext-openmrs` application.properties file (#34)

--- gen_pages.py
+++ gen_pages.py
@@ -8,7 +8,7 @@ ODOO_OPENMRS_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-odoo-
 OPENMRS_ORTHANC_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-openmrs-orthanc/refs/heads/main/openmrs-orthanc/src/main/resources/config/application.properties"
 KEYCLOCK_SUPERSET_GITHUB_URL = ""
 OPENMRS_SENAITE_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-openmrs-senaite/refs/heads/main/senaite-openmrs/src/main/resources/config/application.properties"
-ERPNEXT_OPENMRS_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-erpnext-openmrs/789020a15dbbb10f0bd5b2c994a1de2e82cf7ae7/erpnext-openmrs/src/main/resources/config/application.properties"
+ERPNEXT_OPENMRS_GITHUB_URL = "https://raw.githubusercontent.com/ozone-his/eip-erpnext-openmrs/refs/heads/main/erpnext-openmrs/src/main/resources/config/application.properties"
 
 mkdocs_config_points = {'mkdocs-config-name': 'Name', 'mkdocs-config-description': 'Description',
                         'mkdocs-config-location': 'Location', 'mkdocs-config-possible-values': 'Possible Values',
